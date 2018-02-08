4423a35ef4ee238df2141cc7f7a4147f68bba314
Varun Varman <varunvarman@gmail.com>
2018-02-08 22:22:05 +0530

    Push to GitHub.

---
+++ README.md
@@ -0,0 +1,4 @@
+EmoGiphy
+===
+
+A iOS application to search for .gif files, based on the input query, that is based on Emojis.
---
+++ emogiphy/EmojiCollectionViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  emogiphy
+//
+//  Created by Naveen on 10/05/17.
+//  Copyright © 2017 dietcode. All rights reserved.
+//
+
+import UIKit
+
+class EmojiCollectionViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
